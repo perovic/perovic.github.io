1a15b1a239d8453e551ff06c98b49308ebe38fc4
Boris Perović <boris.perovic@epfl.ch>
2016-02-17 03:22:06 -0800

    First commit, recreated most of the website

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(window).scroll(function(){
+  if($(window).scrollTop() > 500) {
+    $('.navbar-default').fadeIn(300);
+  } else {
+    $('.navbar-default').fadeOut(300);
+  }
+});
+
+$(document).ready(function() {
+
+  // Init the grid for side-projects.
+  Grid.init();
+
+  // Init WOW for awesome scroll animations.
+  new WOW().init();
+
+  // Init text rotator for cool text display.
+  $(".rotate").textrotator({
+    animation: "dissolve",
+    separator: ",",
+    speed: 2000
+  });
+
+  /* This click listener allows the user to scroll to different parts of the page
+   * using the navbar by attaching ids to anchors throughout the page.
+   */
+  $("a.scroll[href^='#']").on('click', function(e) {
+    e.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(this.hash).offset().top - 40
+    }, 1000, function(){
+      window.location.hash = hash;
+    });
+  });
+});
+
+// Parallax scrolling of the jumbotron background
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-0.8*scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
